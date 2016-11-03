a6783f367b55d3374a97fa75b825c9a9db4abb8d
Daniel Trierweiler <trierweiler@dievision.de>
2016-11-03 11:38:32 +0100

    url_for patch for the view context

--- lib/sinicum/multisite/utils.rb
+++ lib/sinicum/multisite/utils.rb
@@ -22,6 +22,7 @@ module ActionDispatch
           alias_method :sincum_path_for, :path_for
         end
 
+        # The path helpers are modified by this
         def path_for(options = nil)
           regexp = %r(^(#{Sinicum::Multisite::Utils.all_root_paths.join("|")})(/|$))
           sincum_path_for(options).sub(regexp, '/')
@@ -36,10 +37,25 @@ module ActionDispatch
         alias_method :sincum_url_for, :url_for
       end
 
+      # The url_for in the controller context is modified by this
       def url_for(options = nil)
         regexp = %r(^(#{Sinicum::Multisite::Utils.all_root_paths.join("|")})(/|$))
         sincum_url_for(options).sub(regexp, '/')
       end
+    end
+  end
+end
+
+module ActionView
+  module RoutingUrlFor
+    unless method_defined?(:sincum_url_for)
+      alias_method :sincum_url_for, :url_for
+    end
+
+    # The url_for in the view context is modified by this
+    def url_for(options = nil)
+      regexp = %r(^(#{Sinicum::Multisite::Utils.all_root_paths.join("|")})(/|$))
+      sincum_url_for(options).sub(regexp, '/')
     end
   end
 end
