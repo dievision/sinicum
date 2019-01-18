e292164ee1113e1a64c9fd696965792340e6d0d0
Daniel Trierweiler <danieltrierweiler@gmail.com>
2019-01-18 13:22:15 +0100

    mgnl_path should use multisite correctly

--- app/helpers/sinicum/mgnl_helper.rb
+++ app/helpers/sinicum/mgnl_helper.rb
@@ -21,7 +21,7 @@ module Sinicum
       elsif key_or_object.is_a?(String)
         path = key_or_object.dup
       end
-      path
+      url_for(path)
     end
 
     def mgnl_link(key_or_object, options = {}, &block)
