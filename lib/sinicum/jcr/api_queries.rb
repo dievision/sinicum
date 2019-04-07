a4bb573ec440d2bb97fb43056870998d2436f0a5
Patrik Metzmacher <metzmacher@dievision.de>
2019-04-07 13:36:40 +0200

    Initialization of HTTPClient on first use
    Previously, the HTTPClient was initialized (and then re-used) during
    the initialization phase. Among others, this prevented the use of
    mocking libraries like Webmock.
    
    Now, the client is lazily initialized when it is first used.
    
    `http_client_reset!` added in order to safely reset the client.

--- lib/sinicum/jcr/api_queries.rb
+++ lib/sinicum/jcr/api_queries.rb
@@ -4,32 +4,46 @@ module Sinicum
   module Jcr
     # Private: Encapsulates the functionality to run queries on the server.
     class ApiQueries
-      @@http_client_mutex = Mutex.new
       class << self
         attr_reader :jcr_configuration
 
         def configure_jcr=(config_hash)
-          @http_client = nil
+          http_client_reset!
           if config_hash
             @jcr_configuration = JcrConfiguration.new(config_hash.dup.with_indifferent_access)
-            if @jcr_configuration.username.present? || @jcr_configuration.password.present?
-              http_client.set_auth(nil, @jcr_configuration.username, @jcr_configuration.password)
-              http_client.www_auth.basic_auth.challenge(@jcr_configuration.base_proto_host_port)
-            end
-            if @jcr_configuration.protocol == "https"
-              http_client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
-            end
           end
         end
 
         def http_client
           # Thread Safety?
           return @http_client if @http_client
-          @@http_client_mutex.synchronize do
+          http_client_mutex.synchronize do
             return @http_client if @http_client
-            @http_client ||= HTTPClient.new
+            @http_client = build_http_client
           end
           @http_client
+        end
+
+        def http_client_reset!
+          http_client_mutex.synchronize do
+            @http_client = nil
+          end
+        end
+
+        private
+
+        def http_client_mutex
+          @http_client_mutex ||= Mutex.new
+        end
+
+        def build_http_client
+          clnt = HTTPClient.new
+          if @jcr_configuration &&
+              (@jcr_configuration.username.present? || @jcr_configuration.password.present?)
+            clnt.set_auth(nil, @jcr_configuration.username, @jcr_configuration.password)
+            clnt.www_auth.basic_auth.challenge(@jcr_configuration.base_proto_host_port)
+          end
+          clnt
         end
       end
     end
